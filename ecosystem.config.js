d3045782b7614cb31112dbbcc3d65903384493fb
Will Hilliard <william.hilliard@outdoorsy.co>
2020-04-17 13:25:18 -0500

    Update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,10 @@ module.exports = {
       name: 'minesweeper',
       script: 'yarn',
       args: 'start',
+      exec_mode: 'fork',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production'
       },
